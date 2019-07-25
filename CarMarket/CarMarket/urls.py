1ed7f8c118914db4eaf4241d82be03851f0ffc6f
mohammed <moh1999sadan@gmail.com>
2019-07-26 02:56:27 +0300

    Update urls.py

--- CarMarket/CarMarket/urls.py
+++ CarMarket/CarMarket/urls.py
@@ -20,6 +20,9 @@ from CarMarket_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home),
-    path('add/', views.add)
-
+    path('add/', views.add),
+    path('contact/', views.contact),
+    path('details/',views.detailes),
+    path('login/',views.login)
+    path('market/',views.market)
 ]
