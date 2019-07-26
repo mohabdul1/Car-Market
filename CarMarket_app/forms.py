c5f55efee08c3dbc98d03bdbf7592e95869ddd01
Ahmed <A.jamal11300@gmail.com>
2019-07-27 01:15:27 +0300

    Create forms.py

---
+++ CarMarket_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Car
+
+class CarForm(forms.ModelForm):
+    picture = forms.ImageField(required=False)
+    class Meta:
+        model = Car
+        fields = ['ad_description', 'car_model', 'mamsha', 'publish_on', 'picture', 'about','cat_type','car_price']
+        widgets = {
+            'publish_on': forms.DateTimeInput(attrs={'type': 'date'})
+        }
