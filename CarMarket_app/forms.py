d429dc8f6b3267593b18639a03c74a9265e17f51
Ahmed <A.jamal11300@gmail.com>
2019-07-27 02:34:44 +0300

    Update forms.py
    change some thing in model

--- CarMarket_app/forms.py
+++ CarMarket_app/forms.py
@@ -5,7 +5,7 @@ class CarForm(forms.ModelForm):
     picture = forms.ImageField(required=False)
     class Meta:
         model = Car
-        fields = ['car_model', 'mamsha', 'publish_on', 'picture', 'about','car_type','car_price']
+        fields = ['ad_description','car_model', 'mamsha', 'publish_on', 'picture', 'about','car_type','car_price']
         widgets = {
             'publish_on': forms.DateTimeInput(attrs={'type': 'date'})
         }
