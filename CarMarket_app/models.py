8a0b7ccf0014b65be3d40d015690d21c0f8f6b3d
Ahmed <A.jamal11300@gmail.com>
2019-07-27 01:51:48 +0300

    Update models.py

--- CarMarket_app/models.py
+++ CarMarket_app/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Car(models.Model):
-    ad_description = models.CharField(max_length=75),
+    #ad_description = models.CharField(max_length=75),
     mamsha = models.IntegerField()
     publish_on = models.DateTimeField(default=timezone.now)
-    added_on = models.DateTimeField(auto_now_add=True)
-    picture = models.ImageField(upload_to='book_pics')
+    picture = models.ImageField(upload_to='car_pics')
     about = models.TextField()
     car_model= models.IntegerField()
-    cat_type = models.IntegerField()
+    car_type = models.IntegerField()
     car_price = models.IntegerField()
