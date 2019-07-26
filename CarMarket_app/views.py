b7e67dcaeda51b0a7a59a909e581654258388f20
Ahmed <A.jamal11300@gmail.com>
2019-07-27 01:15:37 +0300

    Update views.py

--- CarMarket_app/views.py
+++ CarMarket_app/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseRedirect
+from .forms import CarForm
+from .models import Car
+from django.contrib import messages
+from django.urls import reverse
+
 def home(request):
     return render(request,'home.html')
 
 
 def add(request):
-    return render(request, 'add.html')
+    form = CarForm()
+    if request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            Car = form.save(commit=False)
+            if 'picture' in request.FILES:
+                Car.picture = request.FILES['picture']
+            Car.save()            
+            messages.success(request, 'your book have been added succesfully')
+            return HttpResponseRedirect(reverse('home'))
+
+    data = {
+        'form': form
+    }
+    return render(request, 'add.html',data)
 
 
 def contact(request):
