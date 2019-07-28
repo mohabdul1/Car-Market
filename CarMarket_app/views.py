86b486fe0a60538c5337886510642861466d52e7
Ahmed <A.jamal11300@gmail.com>
2019-07-28 20:34:06 +0300

    Update views.py

--- CarMarket_app/views.py
+++ CarMarket_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseRedirect
-from .forms import CarForm
+from .forms import CarForm , ContactForm
 from .models import Car
 from django.contrib import messages
 from django.urls import reverse
@@ -27,15 +27,33 @@ def add(request):
     return render(request, 'add.html',data)
 
 
-def contact(request):
-    return render(request,'contact.html')
+def contact(request): 
+    form = ContactForm()
 
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            body = form.cleaned_data['body']
+            send_email(name, email, body)
+            form = ContactForm()
+            messages.success(request, 'email is sent, we will contact you soon')
+            return HttpResponseRedirect(reverse('contact'))
+    data = { 
+        'form': form
+    }
+    return render(request, 'contact.html', data)
+
+def send_email(name, email, body):
+    print('sending email done')
 
 def detailes(request):
     return render (request, 'details.html')
 
 
 def login(request):
+
     return render(request,'login.html')
 
 
