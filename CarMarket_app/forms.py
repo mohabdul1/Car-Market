3d0fe5c006c6d36774e993906c07b282465b4533
Ahmed <A.jamal11300@gmail.com>
2019-07-28 20:34:02 +0300

    Update forms.py

--- CarMarket_app/forms.py
+++ CarMarket_app/forms.py
@@ -9,3 +9,16 @@ class CarForm(forms.ModelForm):
         widgets = {
             'publish_on': forms.DateTimeInput(attrs={'type': 'date'})
         }
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=50, label='Full Name')
+    email = forms.EmailField(required=True)
+    body = forms.CharField(widget=forms.Textarea, help_text='write your message here')
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name == 'ahmed':
+            raise forms.ValidationError('ommm, sorry you cann\' send us message')
+
+        return name
